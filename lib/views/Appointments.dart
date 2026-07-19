f2f8ed150c118d18af0ce6f6b59cff6c0e935e9a
amal86912 <amalmaalim25@gmail.com>
2026-07-19 01:59:38 -0700

    hava resolved the errors for the dashboard

--- lib/views/Appointments.dart
+++ lib/views/Appointments.dart
@@ -1,5 +1,4 @@
-// ignore: file_names
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class Appointmentsscreens extends StatefulWidget {
   const Appointmentsscreens({super.key});
@@ -9,91 +8,81 @@ class Appointmentsscreens extends StatefulWidget {
 }
 
 class _AppointmentsState extends State<Appointmentsscreens> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-                title: Text("Appointments"),
-                backgroundColor: Colors.cyan,
-                centerTitle: true,
-               
-        ),
-        body: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                        Text("Appointments", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                        SizedBox(height: 20),
-                        Card(
-                          child: ListTile(
-                            title: Text("Today's Appointment", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            subtitle: Text("Details of appointment"),
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Card(
-                            child: ListTile(
-                            title: Text("Amal maalim", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            subtitle: Text("9.00 A.M"),
-                            ),
-
-
-                        ),
-                        SizedBox(height: 20),
-                        Card(
-                            child: ListTile(
-                            title: Text("Feisal Maalim", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            subtitle: Text("10:30-11:30 A.M"),
-                            )
-                        ),
-                        SizedBox(height: 20),
-                        Card(
-                            child: ListTile(
-                            title: Text("Zumar Maalim", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                            subtitle: Text("11:30-12:30 A.M"),
-                            )
-                        ),
-                        SizedBox(height: 30),
-                        Row(
-                            mainAxisAlignment: 
-                            MainAxisAlignment.center,
-                            children: [
-                                MaterialButton(
-                                    color: Colors.cyan,
-                                    minWidth: 200,
-                                    height: 50,
-                                    onPressed: () {},
-                                    child: Text(
-                                        "Add Appointment",
-                                    style:TextStyle(color: Colors.white),
-                                    ),
-                                ),
-                    
-                    ],
-                  )
-                    ],
+      appBar: AppBar(
+        title: Text("Appointments"),
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Appointments",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                title: Text(
+                  "Today's Appointment",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("Details of appointment"),
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                title: Text(
+                  "Amal maalim",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("9.00 A.M"),
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                title: Text(
+                  "Feisal Maalim",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("10:30-11:30 A.M"),
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                title: Text(
+                  "Zumar Maalim",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text("11:30-12:30 A.M"),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  color: Colors.cyan,
+                  minWidth: 200,
+                  height: 50,
+                  onPressed: () {},
+                  child: Text(
+                    "Add Appointment",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-                    
-
-
-
-                            
-
-
-                        
-
-
-
-
-                      
-                      
-                      
--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,57 +15,52 @@ class _dashboardscreenState extends State<dashboardscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        title: Text("Dashboard"),
         backgroundColor: Colors.cyan,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const Text(
-              "Welcome Back!",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.people, color: Colors.cyan),
-                title: const Text("Patients"),
-                subtitle: const Text("250 Registered"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsetsGeometry.all(20),
+          child: Column(
+            children: [
+              Text(
+                "Welcome Back!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 15),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.calendar_today, color: Colors.green),
-                title: const Text("Appointments"),
-                subtitle: const Text("35 Today"),
+              SizedBox(height: 15),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.calendar_today, color: Colors.green),
+                  title: Text("Appointments"),
+                  subtitle: Text("35 Today"),
+                ),
               ),
-            ),
-            const SizedBox(height: 15),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.access_time, color: Colors.orange),
-                title: const Text("Current Queue"),
-                subtitle: const Text("12 Patients Waiting"),
+              SizedBox(height: 10),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.access_time, color: Colors.orange),
+                  title: Text("Appointments"),
+                  subtitle: Text("12 patients waiting"),
+                ),
               ),
-            ),
-            const SizedBox(height: 40),
-            MaterialButton(
-              color: Colors.cyan,
-              minWidth: 200,
-              height: 50,
-              onPressed: () {
-                Get.toNamed("/queue");
-              },
-              child: const Text(
-                "View Queue",
-                style: TextStyle(color: Colors.white),
+              SizedBox(height: 40),
+              MaterialButton(
+                color: Colors.cyan,
+                minWidth: 200,
+                height: 50,
+
+                onPressed: () {
+                  Get.toNamed("/Queue");
+                },
+                child: Text(
+                  "View Queue",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
